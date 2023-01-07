952c690c267b864334e0e2dc760476bc45caad03
Admin <qayumovfahriddin73@gmail.com>
2023-01-07 20:21:01 +0300

    int main projec

--- Main.java
+++ Main.java
@@ -2,9 +2,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println(sum(5, 7));
+        System.out.println(sum1(3,4));
     }
 
     public static int sum(int a, int b){
         return a + b;
+    }
+    public static int sum1(int c, int d){
+        return c * d;
     }
 }
